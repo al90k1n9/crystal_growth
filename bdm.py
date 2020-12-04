major bug fix : time axis changed to number of monolayers axis. possible bug in standard deviation definition to be corrected later

bdm.py
@@ -4,16 +4,19 @@ from tqdm import tqdm
 from matplotlib import pyplot as plt
 import os
 
-def drop_one_particle(crystal):
+def drop_one_particle(crystal, particle_number):
+    global L
+    if (particle_number//(10*L))%2 == 1: particle_presence_value = 1
+    else: particle_presence_value = 2
     x_dim, y_dim= np.shape(crystal)        #getting the dimensions of the crystal
     col = rd.randint(0,y_dim-1)             #choosing a random column simulating a random deposition of particles
     if crystal[0,col] == 0:                 #makes sure that the chosen column is not already full
         for row in range(x_dim):
             if row == x_dim - 1:            #the particle has reached the bottom of the substrate
-                crystal[row,col] = 1
+                crystal[row,col] = particle_presence_value
                 break
-            if crystal[row + 1, col] == 1 or (row != 0 and crystal[row, (col+1)%y_dim] == 1) or (row!=0 and crystal[row, (col-1)%y_dim] == 1):
-                crystal[row,col] = 1
+            if crystal[row + 1, col] != 0 or (row != 0 and crystal[row, (col+1)%y_dim] != 0) or (row!=0 and crystal[row, (col-1)%y_dim] != 0):
+                crystal[row,col] = particle_presence_value
                 break
         limit_condition = False
         if row == 1:
@@ -29,34 +32,33 @@ def drop_one_particle(crystal):
     return(crystal, limit_condition)
 
 
-def diffuse(crystal):
+def peek_in_diffuse(crystal):
     rows, cols = np.shape(crystal)
-    print(rows, cols)
     diffusion_order = np.arange(cols)
     rd.shuffle(diffusion_order)
-    print(diffusion_order)
     diffusion_range = 1
     for col in diffusion_order:
         h = height_per_column(crystal[:,col])
         h_plus = height_per_column(crystal[:,(col+1)%cols])
         h_minus = height_per_column(crystal[:,col-1])
-        print(h, h_minus, h_plus)
-        if crystal[(rows-h)%rows, col] < diffusion_range+1 and h != 0:
-            if (h_minus < h and h_plus < h):
-                if rd.random()>0.5:
+        if h!=0:
+            if crystal[rows-h, col] < diffusion_range+1:
+                if h_plus < h-1 and h_minus < h-1:
+                    if h_plus < h_minus: crystal[rows-h_plus-1,(col+1)%cols] = 1 + crystal[rows-h,col]
+                    elif h_minus < h_plus :crystal[rows-h_minus-1,(col-1)] = 1 + crystal[rows-h,col]
+                    else:
+                        if rd.random()<0.5 : crystal[rows-h_plus-1,(col+1)%cols] = 1 + crystal[rows-h,col]
+                        else : crystal[rows-h_minus-1,(col-1)] = 1 + crystal[rows-h,col]
+                    crystal[rows-h,col] = 0
+                    continue
+                if h_minus < h-1:
+                    crystal[rows-h_minus-1,(col-1)] = 1 + crystal[rows-h,col]
+                    crystal[rows-h,col] = 0
+                    continue
+                if h_plus < h-1:
                     crystal[rows-h_plus-1,(col+1)%cols] = 1 + crystal[rows-h,col]
-                else :
-                    crystal[rows-h_minus-1, col-1] = 1 + crystal[rows-h,col]
-                crystal[rows-h,col] = 0
-                continue
-            if h_minus < h:
-                crystal[rows-h_minus-1, col-1] = 1 + crystal[rows-h,col]
-                crystal[rows-h,col] = 0
-                continue
-            if h_plus < h:
-                crystal[rows-h_plus-1,(col+1)%cols] = 1 + crystal[rows-h,col]
-                crystal[rows-h,col] = 0
-                continue
+                    crystal[rows-h,col] = 0
+                    continue
     return crystal
 
 def height_per_column(col):
@@ -65,6 +67,9 @@ def height_per_column(col):
             return (len(col) - i)
     return 0
 
+def deviation(x):
+    return abs(np.mean(x - x.mean()))
+
 def height_analysis(crystal):       #returns std dev and mean height of the crystal
     column_heights = []
     for col in range(L):
@@ -72,7 +77,7 @@ def height_analysis(crystal):       #returns std dev and mean height of the crys
 
     column_heights = np.asarray(column_heights)
     mean_height = np.mean(column_heights)
-    std_height = np.std(column_heights)
+    std_height = deviation(column_heights)
     return(mean_height, std_height, column_heights)
 
 
@@ -92,38 +97,32 @@ def correlation_r(heights):
 ####################################################################################################
 ####################################################################################################
 
-monolayers = 20
-L=10
+monolayers = 700
+L=300
 n_particles = monolayers * L
 y_dim = 3
 std_height_time = np.zeros(n_particles)
-routines = 1
+routines = 100
 #os.chdir("/home/algoking/Documents/M2/crystal_growth/output")
 #file = open("crystal.dat", "w")
 correlation_t = np.zeros([L,n_particles])
 for iteration in tqdm(range(routines)):
     crystal = np.zeros([y_dim, L])
     for i in tqdm(range(n_particles)):
-        crystal, error = drop_one_particle(crystal)
+        crystal, error = drop_one_particle(crystal,i)
         if error:
             print("Error!! number of particles deposited so far: ", i-1)
             break
+        #crystal = peek_in_diffuse(crystal)
         mean, std, column_heights = height_analysis(crystal)
-        g_r = correlation_r(column_heights)
-        correlation_t[:,i] = g_r
-        #std_height_time[i]+=std
-"""file.close()
-os.chdir("/home/algoking/Documents/M2/crystal_growth/")"""
-#std_height_time = std_height_time/routines
-#plt.imshow(crystal)
-#plt.show()
-print(correlation_t)
+        std_height_time[i]+=std
+std_height_time = std_height_time/routines
+"""plt.imshow(crystal)
+plt.show()"""
 
 os.chdir("/home/algoking/Documents/M2/crystal_growth/output")
-file = open("cor_(100,200,1)", "w")
-for row in range(np.shape(correlation_t)[0]):
-    for col in range(np.shape(correlation_t)[1]):
-        file.write(str(correlation_t[row,col])+" ")
-    file.write("\n")
+file = open("w_vs_t_(" + str(L) + ", " + str(monolayers)+", " + str(routines)+")", "w")
+for index in range(len(std_height_time)):
+    file.write(str(index) + " " + str(std_height_time[index]) + "\n" )
 file.close()
 os.chdir("/home/algoking/Documents/M2/crystal_growth/")

output/correlation_grapher.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 from matplotlib.animation import FuncAnimation
 plt.style.use('seaborn-pastel')
 
@@ -27,10 +28,11 @@ def animate(i):
     y = correlation_t[:,i]
     line.set_data(x, y)
     fig.legend("t=" + str(i))
+    print(i)
     return line,
 
 anim = FuncAnimation(fig, animate, init_func=init, frames=np.shape(correlation_t)[1], interval=20, blit=True,repeat = False)
 
 
-#anim.save('correlation.gif', writer='imagemagick')
-plt.show()
+anim.save('correlation(100,200).mp4', writer='ffmpeg')
+#plt.show()

output/fit_n_graph.py
@@ -13,7 +13,7 @@ def test_func(x, beta, alpha):
 
 def interface_width(n_particles, std_height_time, show_params = False, data = True, data_fit = True):
     global L
-    xlist = np.arange(n_particles)
+    xlist = np.arange(n_particles)/L
     best_param, covar = cf(test_func, xlist, std_height_time,p0 = [1, 1])
     tx = L**(best_param[1]/best_param[0])
     if show_params:
@@ -21,14 +21,13 @@ def interface_width(n_particles, std_height_time, show_params = False, data = Tr
         #print("t0: ", tx)
         #print("z: ",np.log(tx-L))
         #print(covar)
-    if data: plt.plot(xlist/(L**best_param[1]),std_height_time/(L**best_param[1]))
-    if data_fit:
-        #xlist = xlist/(L**best_param[0])
-        plt.plot(xlist, test_func(xlist, best_param[0], best_param[1]))
+
+    if data: plt.plot(xlist/tx,std_height_time/(L**best_param[1]))
+    if data_fit: plt.plot(xlist/tx, test_func(xlist, best_param[0], best_param[1])/(L**best_param[1]))
 
 
 prefix = "w_vs_t_"
-suffix = [(80,200,100), (90,200,100), (100,200,100), (120,300,100), (150,200,100)]
+suffix = [(50,200,100), (100,300,100), (200,500,100), (300,700,100)]
 plot_legend=[]
 for elem in suffix:
     filename = prefix + str(elem)
@@ -42,7 +41,10 @@ for elem in suffix:
 plt.yscale("log")
 plt.xscale("log")
 plt.legend(plot_legend)
-os.chdir("/home/algoking/Documents/M2/Crystal_growth/results")
-plt.savefig("comp_python.png")
-os.chdir("/home/algoking/Documents/M2/Crystal_growth")
+plt.xlabel("Number of monolayers/cross-over time")
+plt.ylabel("w/saturation_value")
+
+os.chdir("/home/algoking/Documents/M2/crystal_growth/results")
+plt.savefig("w_monolayers_L_50_300_logscale_collapse.png")
+#os.chdir("/home/algoking/Documents/M2/crystal_growth")
 plt.show()