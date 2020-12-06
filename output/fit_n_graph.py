6771a9ce555471eb3865dee578b2387e351c7783
SK <karthikeyan98pdy@gmail.com>
2020-12-06 17:52:26 +0100

    regular updates

--- output/fit_n_graph.py
+++ output/fit_n_graph.py
@@ -6,37 +6,36 @@ import os
 def heavy_side(x):                          #step function
     return x>0
 
-def test_func(x, beta, alpha):
+def test_func(x, beta, alpha, gamma):
     global L
-    tx = L**(alpha/beta)
+    tx = gamma*L**(alpha/beta)
     return heavy_side(tx-x)*(x**beta) + heavy_side(x-tx)*(L**alpha)
 
-def interface_width(n_particles, std_height_time, show_params = False, data = True, data_fit = True):
+def interface_width(xlist, std_height_time, show_params = False, data = True, data_fit = True):
     global L
-    xlist = np.arange(n_particles)/L
-    best_param, covar = cf(test_func, xlist, std_height_time,p0 = [1, 1])
-    tx = L**(best_param[1]/best_param[0])
+    best_param, covar = cf(test_func, xlist, std_height_time)
+    tx = best_param[2]*L**(best_param[1]/best_param[0])
     if show_params:
         print(best_param, np.log(tx-L))
         #print("t0: ", tx)
         #print("z: ",np.log(tx-L))
         #print(covar)
 
-    if data: plt.plot(xlist/tx,std_height_time/(L**best_param[1]))
+    if data: plt.plot(xlist,std_height_time)
     if data_fit: plt.plot(xlist/tx, test_func(xlist, best_param[0], best_param[1])/(L**best_param[1]))
 
 
 prefix = "w_vs_t_"
-suffix = [(50,200,100), (100,300,100), (200,500,100), (300,700,100)]
+suffix = [(50,2500,100), (100,5000,100), (200,10000,100), (500,25000,100),(800,40000,100),(1000,50000,100)]
 plot_legend=[]
 for elem in suffix:
     filename = prefix + str(elem)
     ylist = np.loadtxt(filename)
-    ylist = ylist[:,1]
+    xlist, ylist = ylist[:,0] , ylist[:,1]
     L = elem[0]
     plot_legend.append("L=" + str(L))
     n_particles = elem[0] * elem[1]
-    interface_width(n_particles, ylist, data_fit=False)
+    interface_width(xlist, ylist, data_fit=False)
 
 plt.yscale("log")
 plt.xscale("log")
