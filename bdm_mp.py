e63e2a2a1cfd95b42dcb42091d2842dcb4a567ac
SK <karthikeyan98pdy@gmail.com>
2020-12-17 07:01:17 +0100

    modified to save w of all routines

--- bdm_mp.py
+++ bdm_mp.py
@@ -6,8 +6,17 @@ import numpy as np
 import random as rd
 from datetime import datetime
 
-start = datetime.now()
-L = 800
+def correlation(time, heights):
+    mean_height = np.mean(heights)
+    g = []
+    g.append(time)
+    for r in range(-L//2, L//2):
+        g.append(np.mean((np.roll(heights, -r)-mean_height) * (heights-mean_height)))
+    return np.asarray(g)
+
+
+
+L = 100
 monolayers = L*50
 n_particles = L*monolayers
 routines = 100
@@ -15,6 +24,7 @@ routines = 100
 #crystal = np.zeros([monolayers*4,L])
 #crystal[np.shape(crystal)[0]-(int(heights[col])), col] = 1
 
+std_height_time_matrix = np.zeros([monolayers, routines])
 
 def one_deposition():
     global L
@@ -26,7 +36,7 @@ def one_deposition():
         col = rd.randint(0,L-1)
         heights[col] = max(heights[col]+1, heights[col-1], heights[(col+1)%L])
         if i%L == 0:
-            std_height_time[std_index] += np.std(heights)
+            std_height_time[std_index] = np.std(heights)
             std_index+=1
     return std_height_time
 
@@ -34,15 +44,17 @@ pool = mp.Pool(mp.cpu_count())
 results = [pool.apply_async(one_deposition, args=()) for iteration in range(routines)]
 Results = [elem.get() for elem in results]
 pool.close()
-std_height_time = np.zeros(n_particles//L)
-for elem in Results:
-    std_height_time += elem
-std_height_time = std_height_time/routines
+for index in range(len(Results)):
+    std_height_time_matrix[:,index] = Results[index]
+
+
 
 
 os.chdir("/home/algoking/Documents/M2/crystal_growth/output")
-file = open("w_vs_t_(" + str(L) + ", " + str(monolayers) + ", " + str(routines) + ")", "w")
-for index in range(len(std_height_time)):
-    file.write(str(index) + " " + str(std_height_time[index])+"\n")
+file = open("all_routines_(" + str(L) + ", " + str(monolayers) + ", " + str(routines) + ")", "w")
+for index in range(np.shape(std_height_time_matrix)[0]):
+    file.write(str(index + 1) + " ")
+    for col in range(np.shape(std_height_time_matrix)[1]):
+        file.write(str(std_height_time_matrix[index,col])+" ")
+    file.write("\n")
 file.close()
-print(datetime.now()-start)
