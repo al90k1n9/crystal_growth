e64cda9548243152641606eeba4ab3c89d898a71
SK <karthikeyan98pdy@gmail.com>
2020-12-17 07:02:00 +0100

    modified to calc correlation

--- bdm_revised.py
+++ bdm_revised.py
@@ -7,29 +7,33 @@ from datetime import datetime
 
 start = datetime.now()
 L = 1000
-monolayers = 2200
+monolayers =75
 n_particles = L*monolayers
-routines = 1
+routines = 500
+correlation_time_stamps = np.arange(1,72,10) #np.concatenate((correlation_time_stamps,np.asarray([500,900])))
 
 #crystal = np.zeros([monolayers*4,L])
 std_height_time = np.zeros(monolayers)
-correlation = []
 time = []
+correlation_matrix = np.zeros([len(correlation_time_stamps),L+1])
 
 for iteration in tqdm(range(routines)):
+    correlation = []
     heights = np.zeros(L)
     std_index=0
     for i in tqdm(range(n_particles)):
         col = rd.randint(0,L-1)
         heights[col] = max(heights[col]+1, heights[col-1], heights[(col+1)%L])
         #crystal[np.shape(crystal)[0]-(int(heights[col])), col] = 1
-        if i/L in np.arange(10,2200,250):
+        if i/L in correlation_time_stamps:
             mean_height = np.mean(heights)
             g = []
-            time.append(i/L)
+            g.append(i/L)
             for r in range(-L//2, L//2):
                 g.append(np.mean((np.roll(heights, -r)-mean_height) * (heights-mean_height)))
             correlation.append(g)
+    correlation_matrix += np.asarray(correlation)
+correlation = np.asarray(correlation_matrix)/routines
 
 #std_height_time = std_height_time/routines
 """for i in range(len(correlation)):
@@ -48,7 +52,7 @@ print(datetime.now()-start)"""
 print(np.shape(correlation))
 col = -L/2
 os.chdir("/home/algoking/Documents/M2/crystal_growth/output")
-file = open("correlation(" + str(L) + ", " + str(monolayers) + ", " + str(routines) + ")", "w")
+file = open("correlation_avenorm_["+ str(correlation_time_stamps[0]) +","+ str(correlation_time_stamps[-1])+"]_(" + str(L) + ", " + str(routines) + ")", "w")
 for index in range(np.shape(correlation)[0]):
     for g_r in range(np.shape(correlation)[1]):
         file.write(str(correlation[index][g_r])+" ")
