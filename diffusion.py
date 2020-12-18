45d56d38d332766f57b5e3882234f767387f9496
SK <karthikeyan98pdy@gmail.com>
2020-12-18 20:30:36 +0100

    prototype of diffusion wo any analysis part yet

--- diffusion.py
+++ diffusion.py
@@ -7,47 +7,46 @@ import random as rd
 from datetime import datetime
 
 L = 10
-monolayers = 10
+monolayers = 3
 n_particles = L*monolayers
 routines = 1
 diff_range = 3
 
+calc_correlation = False
+calc_w = True
+
+crystal = np.zeros([L, monolayers * 4])
+
 #crystal[np.shape(crystal)[0]-(int(heights[col])), col] = 1
+
+def find_min_col(heights, col):
+    global diff_range, L
+    min_col = col
+    for i in range(1, diff_range + 1):
+        if heights[(col+i)%L] == heights[col-i]:
+            continue
+        if heights[(col+i)%L]<heights[col]:
+            min_col = (col+i)%L
+        if heights[col-i]<heights[col]:
+            min_col = col-i
+    return min_col
 
 
 def one_deposition():
-    global L
-    global n_particles
-    std_height_time = np.zeros(monolayers)
+    global L, monolayers, n_particles, diff_range, crystal
+    #std_height_time = np.zeros(monolayers)
     heights = np.zeros(L)
-    std_index=0
-    for i in tqdm(range(n_particles)):
+    std_index = 0
+    for i in range(n_particles):
         col = rd.randint(0,L-1)
-        local_heights=[]
-        for i in range(col-diff_range, col+diff_range+1):
-            local_heights.append(heights[i])
-        min_col = local_heights.index(min(local_heights))
+        min_col = find_min_col(heights, col)
         heights[min_col] += 1
-        """if i%L == 0:
-            std_height_time[std_index] += np.std(heights)
-            std_index+=1"""
-    return heights
+        print(col,min_col, heights)
+        crystal[np.shape(crystal)[0] - int(heights[min_col]), min_col] = 1
 
-pool = mp.Pool(mp.cpu_count())
-results = [pool.apply_async(one_deposition, args=()) for iteration in range(routines)]
-Results = [elem.get() for elem in results]
-pool.close()
-std_height_time = np.zeros(n_particles//L)
-"""for elem in Results:
-    std_height_time += elem
-std_height_time = std_height_time/routines"""
-plt.imshow(Results[0])
+    if calc_w:
+        return crystal
+
+crystal = one_deposition()
+plt.imshow(crystal)
 plt.show()
-
-
-"""os.chdir("/home/algoking/Documents/M2/crystal_growth/output")
-file = open("w_vs_t_(" + str(L) + ", " + str(monolayers) + ", " + str(routines) + ")", "w")
-for index in range(len(std_height_time)):
-    file.write(str(index) + " " + str(std_height_time[index])+"\n")
-file.close()
-print(datetime.now()-start)"""
